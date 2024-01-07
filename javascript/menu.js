c18d1306191b2c04725b9e177dafb60d54d781dd
emilie <emve0001@stud.kea.dk>
2024-01-07 17:55:25 +0100

    firstcommitotd

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,14 +1,14 @@
+//////BURGER-MENU////////
 const burger = document.querySelector(".burger");
 const nav = document.querySelector("nav");
-const menu = document.querySelector(".menu");
-const links = document.querySelectorAll(".menu li a");
+const menuLinks = document.querySelectorAll(".menu li a");
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
   nav.classList.toggle("active");
 });
 
-links.forEach((link) => {
+menuLinks.forEach((link) => {
   link.addEventListener("click", () => {
     burger.classList.remove("active");
     nav.classList.remove("active");
